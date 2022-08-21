f21242cb2063f20f2aa06a3c64e09a27618a2238
Nicolás Órdenes <nicordenes@gmail.com>
2022-08-21 01:24:19 -0400

    Add Game#play acceptance test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -15,4 +15,18 @@ describe Game do
       end
     end
   end
+
+  describe '#play' do
+    it 'plays a complete game' do
+      game = described_class.new
+      players = game.players
+
+      allow(players[0]).to receive(:get_input).and_return('a1', 'a2', 'a3')
+      allow(players[1]).to receive(:get_input).and_return('b1', 'b2', 'b3')
+
+      expect do
+        game.play
+      end.to output(/GAME OVER/).to_stdout
+    end
+  end
 end
