a110c3b0b90c332b339f76194e272d4f78abf695
Nicolás Órdenes <nicordenes@gmail.com>
2022-08-21 00:07:41 -0400

    Add Board #rows and #cols attr_readers

--- lib/board/board.rb
+++ lib/board/board.rb
@@ -1,5 +1,9 @@
 class Board
+  attr_reader :rows, :cols
+
   def initialize(rows, cols = rows)
+    @rows = rows
+    @cols = cols
     @board = Array.new(rows) { Array.new(cols, ' ') }
 
     @printer = BoardPrinter.new
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,4 +1,34 @@
 describe Board do
+  describe'#rows' do
+    it 'returns the number of rows for a symmetrical board' do
+      rows = 5
+      board = Board.new(rows)
+      expect(board.rows).to eq(rows)
+    end
+
+    it 'returns the number of rows for an asymmetrical board' do
+      rows = 5
+      cols = 3
+      board = Board.new(rows, cols)
+      expect(board.rows).to eq(rows)
+    end
+  end
+
+  describe'#cols' do
+    it 'returns the number of rows for a symmetrical board' do
+      rows = 5
+      board = Board.new(rows)
+      expect(board.cols).to eq(rows)
+    end
+
+    it 'returns the number of rows for an asymmetrical board' do
+      rows = 5
+      cols = 3
+      board = Board.new(rows, cols)
+      expect(board.cols).to eq(cols)
+    end
+  end
+
   describe '#show' do
     let(:board) { described_class.new(3) }
 
