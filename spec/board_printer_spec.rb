15a92a60ad2ceb3fc63248ceadb69f7ebe7faf8d
Nicolás Órdenes <nicordenes@gmail.com>
2022-08-20 22:53:49 -0400

    Remove BoardPrinter test
    Remove simpler test to reduce maintenance.
    
    Every heredoc has to be updated after a change in the output format,
    making the test too fragile.
    
    A the more general test was left in place, since covers most of the
    expected cases.

--- spec/board_printer_spec.rb
+++ spec/board_printer_spec.rb
@@ -2,72 +2,20 @@ describe BoardPrinter do
   let(:board_printer) { described_class.new }
 
   describe '#show' do
-    context 'single cell board' do
-      context 'on an empty board' do
-        it 'displays a single cell' do
-          board_printer.board = [[nil]]
-          expected = <<~out
-              a
-            +---+
-            |   |
-            +---+
-          out
+    it 'displays a two cell board cell' do
+      board_printer.board = [['x', nil], [nil, 'o']]
+      expected = <<~out
+          a   b
+        +---+---+
+        | x |   |
+        +---+---+
+        |   | o |
+        +---+---+
+      out
 
-          expect do
-            board_printer.show
-          end.to output(expected).to_stdout
-        end
-      end
-
-      context 'on an non-empty board' do
-        it 'displays a single cell' do
-          board_printer.board = [['x']]
-          expected = <<~out
-              a
-            +---+
-            | x |
-            +---+
-          out
-
-          expect do
-            board_printer.show
-          end.to output(expected).to_stdout
-        end
-      end
-    end
-
-    context 'two cell board' do
-      it 'displays a two cell board cell' do
-        board_printer.board = [['x', nil]]
-        expected = <<~out
-            a   b
-          +---+---+
-          | x |   |
-          +---+---+
-        out
-
-        expect do
-          board_printer.show
-        end.to output(expected).to_stdout
-      end
-    end
-
-    context 'square board' do
-      it 'displays a two cell board cell' do
-        board_printer.board = [['x', nil], [nil, 'o']]
-        expected = <<~out
-            a   b
-          +---+---+
-          | x |   |
-          +---+---+
-          |   | o |
-          +---+---+
-        out
-
-        expect do
-          board_printer.show
-        end.to output(expected).to_stdout
-      end
+      expect do
+        board_printer.show
+      end.to output(expected).to_stdout
     end
   end
 end
